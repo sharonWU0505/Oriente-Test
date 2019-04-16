c05a988477a43369768560fe80b2b52aa48b6397
sharonWU0505 <astrumstella048@gmail.com>
2019-04-16 16:44:55 +0800

    Add username and password

--- get_products.sh
+++ get_products.sh
@@ -4,10 +4,8 @@ main_page=https://f2e-test.herokuapp.com
 login_api=https://f2e-test.herokuapp.com/api/auth
 product_api=https://f2e-test.herokuapp.com/api/products?limit=1000
 
-echo "Please enter the username."
-read username
-echo "Please enter the password."
-read -s password
+username=f2e-candidate
+password=P@ssw0rd
 
 csrf=$(curl -L $main_page -c cookie.txt | grep -o 'csrf:.*\"}' | cut -d ':' -f 2 | tr -d '"}')
 curl -X POST -d "username=$username&password=$password&csrf=$csrf" $login_api -b cookie.txt -c cookie.txt
